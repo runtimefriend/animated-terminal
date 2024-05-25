08fa3eea1b5a9008eb7f8e33c027279e8167c2ad
runtimefriend <runtimefriend@gmail.com>
2024-05-25 16:10:25 +0900

    some cleanup, styling, remove tailwind

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const TYPE_SPEED_WIDTH = 10
 const TYPE_SPEED_UNIT = ( Math.PI * 2 ) / 10
 
 
-const PRINT_SPEED = 385
+const PRINT_SPEED = 185
 const PRINT_SPEED_VARIATION = 30
 
 let username = "main@ubuntu"
@@ -16,8 +16,8 @@ let username = "main@ubuntu"
 
 let wait = async (ms) => {
     return new Promise((resolve, _) => {
-       setTimeout(() => resolve('timeout succeeded'), ms);
-    });
+       setTimeout(() => resolve('timeout succeeded'), ms)
+    })
 }
 
 let inputElement = document.querySelector('#terminal__input')
@@ -26,39 +26,31 @@ let terminalOutput = ""
 
 let cursorIndex = 0
 
-let addToInputAndWait = async (content) => {
-    //if (content) {
-        return new Promise((resolve, _) => {
-            /*if( content == ' '){
-                content = "&nbsp;"
-            }*/
-            //inputElement.innerHTML = (inputElement.innerHTML + content).replace("<cursor></cursor>", "") + "<cursor></cursor>"
-            if(cursorIndex==0){
-                terminalOutput = terminalOutput + content
-                inputElement.innerHTML = terminalOutput + "<cursor></cursor>"
-            }else{
-                let cursorPosition = terminalOutput.length + cursorIndex
+let addToInputAndWait = async (content,speed) => {
+    return new Promise((resolve, _) => {
+        if(cursorIndex==0){
+            terminalOutput = terminalOutput + content
+            inputElement.innerHTML = terminalOutput + "<cursor></cursor>"
+        }else{
+            let cursorPosition = terminalOutput.length + cursorIndex
 
-                terminalOutput =
-                    terminalOutput.substring(0,cursorPosition)
-                    + content
-                    + terminalOutput.substring(cursorPosition)
-                inputElement.innerHTML =
-                    terminalOutput.substring(0,cursorPosition)
-                    + content
-                    + "<cursor>"
-                    + terminalOutput.charAt(cursorPosition)
-                    + "</cursor>"
-                    + terminalOutput.substring(cursorPosition+1)
-            }
-            //inputElement.innerHTML = inputElement.innerHTML.substring( 0 , inputElement.innerHTML.length - 17 ) + content + "<cursor></cursor>"
-            resolve()
-        }).then(() => {
-            return wait(TYPE_SPEED)//+TYPE_SPEED_OSCILLATION*Math.sin( inputElement.length * TYPE_SPEED_UNIT ))
-        })
-    //} else {
-    //    return Promise.resolve()
-    //}
+            terminalOutput =
+                terminalOutput.substring(0,cursorPosition)
+                + content
+                + terminalOutput.substring(cursorPosition)
+            inputElement.innerHTML =
+                terminalOutput.substring(0,cursorPosition)
+                + content
+                + "<cursor>"
+                + terminalOutput.charAt(cursorPosition)
+                + "</cursor>"
+                + terminalOutput.substring(cursorPosition+1)
+        }
+        resolve()
+    }).then(() => {
+        return wait(speed)//+TYPE_SPEED_OSCILLATION*Math.sin( inputElement.length * TYPE_SPEED_UNIT ))
+    })
+
 }
 
 let deleteAtCursor = async () => {
@@ -68,49 +60,22 @@ let deleteAtCursor = async () => {
     })
 }
 
-/*let processCommand = (charArray,i) => {
-    if(charArray[i] == '<'){
-        let commandStart = i+1
-        while(true){
-            i++
-            if (i > charArray.length){
-                break
-            }
-            if (charArray[i] == '>' || charArray[i] == '-') {
-                let command = charArray.slice(start,i).toString()
-                if (charArray[i] == '-'){
-                    let timeStart = i
-                    while(true){
-                        i++
-                        if (i > charArray.length){
-                            break
-                        }
-                        if(charArray[i] == '>'){
-                            //let time = charArray.slice()
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return i
-}*/
 let decrementCursor = async () => {
     cursorIndex -= 1
     return Promise.resolve()
 }
 
-let processCommand = async (command, index) => {
+let processCommand = async (command, index, speedFactor) => {
     let promise = Promise.resolve()
     switch(command){
         case "enter":
             cursorIndex = 0
-            promise = addToInputAndWait("\n")
+            promise = addToInputAndWait("\n",TYPE_SPEED * speedFactor)
             break
         case "left":
             promise = [...Array(Math.abs(index)).keys()].reduce((accum, _) => {
                 return accum.then(() => {
-                    return decrementCursor().then(() => addToInputAndWait(""))
+                    return decrementCursor().then(() => addToInputAndWait("",TYPE_SPEED * speedFactor))
                 })
             }, Promise.resolve())
             break
@@ -120,22 +85,27 @@ let processCommand = async (command, index) => {
     return promise
 }
 
-let typeToTerminal = async (content) => {
+let typeToTerminal = async (content, speedFactor) => {
     // print 1 character at a time with 100ms delay
     let charArray = content.split('')
     return await charArray.reduce((accum, char) => {
-        return accum.then(() => addToInputAndWait(char))
+        return accum.then(() => addToInputAndWait(char,TYPE_SPEED * speedFactor))
     }, Promise.resolve())//.then(() => addToInputAndWait("\n"))
 }
 
-let printToTerminal = async (content) => {
+let printToTerminal = async (content, speedFactor) => {
     // print line by line
     let lineArray = content.split("\n")
     for ( let i = 0 ; i < lineArray.length - 1 ; i++ ){
         lineArray[ i ] += "\n"
     }
     return await lineArray.reduce((accum, line) => {
-        return accum.then(() => {addToInputAndWait(line);terminal__input.scrollTo(0,10000)})//.then(() => addToInputAndWait("\n"))
+        return accum.then(() => {
+            return addToInputAndWait(line,PRINT_SPEED * speedFactor).then(() => {
+                terminal__input.scrollTo(0,10000)
+                return Promise.resolve()
+            })
+        })//.then(() => addToInputAndWait("\n"))
     }, Promise.resolve())
 }
 
@@ -144,6 +114,7 @@ let animateTerminal = (terminalText) => {
         let action = element.action
         let value = ''
         let index = 0
+        let speedFactor = 1.0 / (element.speed || 1)
         if (typeof(element.value) == 'string') {
             value = element.value
         } else if (element.value) {
@@ -154,16 +125,16 @@ let animateTerminal = (terminalText) => {
         }
         return accum.then(() => {
             if (action === 'print') {
-                return printToTerminal(value)
+                return printToTerminal(value, speedFactor)
             } else if (action === 'user') {
                 if ( value ) {
                     username = value
                 }
-                return printToTerminal("<user>" + username + "</user>: <loc>~</loc> $ ")
-            }else if (action === 'type') {
-                return typeToTerminal(value)
+                return printToTerminal("<user>" + username + "</user>: <loc>~</loc> $ ", speedFactor)
+            } else if (action === 'type') {
+                return typeToTerminal(value, speedFactor)
             } else if (action === 'command') {
-                return processCommand( value, index )
+                return processCommand( value, index, speedFactor )
             } else if (action === 'wait') {
                 return wait(parseInt(value))
             } else {
@@ -174,6 +145,8 @@ let animateTerminal = (terminalText) => {
     }, Promise.resolve())
 }
 
-fetch('intro.json')
-    .then(resp => resp.json())
-    .then(jsonData => animateTerminal(jsonData))
+let startAnimateTerminal = () => {
+    fetch('terminal_output.json')
+        .then(resp => resp.json())
+        .then(jsonData => animateTerminal(jsonData))
+}
